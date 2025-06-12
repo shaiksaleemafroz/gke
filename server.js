0161c131dab3815397540f0eec2a8ce3df852f1f
shaik <95034852+Anjalijm@users.noreply.github.com>
2025-06-12 09:24:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 // App
 const app = express();
 app.get('/', function (req, res) {
-  res.send('testing for multiple stages for my application\n');
+  res.send('welcome to the Devops engineering\n');
 });
 
 app.listen(PORT);

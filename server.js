8472f8a5debaa2d7977b2e28f1fcc886a7babc13
shaik <95034852+Anjalijm@users.noreply.github.com>
2025-06-12 20:11:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 // App
 const app = express();
 app.get('/', function (req, res) {
-  res.send('welcome to the Devops engineering\n');
+  res.send('WELCOME TO THE 2025 NEW WORLD\n');
 });
 
 app.listen(PORT);
